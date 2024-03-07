2389a3335cfd84ad8b712a69378290eba7685c06
pedropark99 <pedropark99@gmail.com>
2024-03-06 22:43:21 -0300

    Improved, but not exactly

--- src/roverlap.cpp
+++ src/roverlap.cpp
@@ -25,7 +25,7 @@ SEXP distance_point_to_line_impl(double x_point,
 }
 
 
-double distance(double x1, double y1, double x2, double y2) {
+double distance_(double x1, double y1, double x2, double y2) {
   double s1 = pow(x2 - x1, 2.0);
   double s2 = pow(y2 - y1, 2.0);
   return sqrt(s1 + s2);
@@ -41,6 +41,16 @@ bool __point_overlap_line_impl(double x_point,
     double y1_line = y_line[0];
     double y2_line = y_line[1];
     
+    double dist_to1 = distance_(x_point, y_point, x1_line, y1_line);
+    if (dist_to1 < distance) {
+      return true;
+    }
+    
+    double dist_to2 = distance_(x_point, y_point, x2_line, y2_line);
+    if (dist_to2 < distance) {
+      return true;
+    }
+    
     double a = (y2_line - y1_line);
     double b = (x2_line - x1_line);
     double c = x1_line * y2_line - x2_line * y1_line;
@@ -48,7 +58,6 @@ bool __point_overlap_line_impl(double x_point,
     double d1 = abs(a * x_point + b * y_point + c);
     double d2 = sqrt(a * a + b * b);
     double d = d1 / d2;
-    
     if (d < distance) {
         return true;
     }
